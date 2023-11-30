2561888e2a347f87e10b810e793249dcca38cb45
Ahmedmansor <51694719+Ahmedmansor@users.noreply.github.com>
2023-11-30 04:09:30 +0200

    first commit

---
+++ lib/AppRoot/app_root.dart
@@ -0,0 +1,14 @@
+import 'package:bookly_app/features/splash/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AppRoot extends StatelessWidget {
+  const AppRoot({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: SplashView(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:bookly_app/app_root.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const AppRoot());
+}
