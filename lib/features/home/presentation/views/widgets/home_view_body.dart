ef5669c88022a3c683ea1196bb20935d15a0131f
Ahmedmansor <51694719+Ahmedmansor@users.noreply.github.com>
2023-12-03 01:05:07 +0200

    create styles file and add title to home view

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:bookly_app/core/utils/app_setting.dart';
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_list_view_horizontal.dart';
 import 'package:flutter/material.dart';
@@ -7,11 +9,22 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CustomAppBar(),
-        CustomListViewHor(),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const CustomAppBar(),
+          const CustomListViewHor(),
+          SizedBox(
+            height: AppSettings.defultSize * 3,
+          ),
+          const Text(
+            'Best Seller',
+            style: Styles.titleMedium,
+          ),
+        ],
+      ),
     );
   }
 }
