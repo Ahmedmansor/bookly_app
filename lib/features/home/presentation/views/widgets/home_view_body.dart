5ec19d5ece73612110748492e1a723eaf80cbbf4
Ahmedmansor <51694719+Ahmedmansor@users.noreply.github.com>
2023-12-03 04:17:49 +0200

    best seller item part three

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,8 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/app_setting.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_list_view_horizontal.dart';
 import 'package:flutter/material.dart';
@@ -10,68 +12,24 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const CustomAppBar(),
-          const CustomListViewHor(),
+          CustomAppBar(),
+          CustomListViewHor(),
           SizedBox(
-            height: AppSettings.defultSize * 3,
+            height: 50,
           ),
-          const Text(
+          Text(
             'Best Seller',
             style: Styles.textStyle18,
           ),
-          const SizedBox(
+          SizedBox(
             height: 20,
           ),
-          const BestSellerListViewItem(),
-        ],
-      ),
-    );
-  }
-}
-
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 125,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.5 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  image: const DecorationImage(
-                      image: AssetImage(
-                        AssetsData.testImage,
-                      ),
-                      fit: BoxFit.fill),
-                  color: Colors.red),
-            ),
-          ),
-          const SizedBox(
-            width: 30,
-          ),
-          Column(
-            children: [
-              SizedBox(
-                width: AppSettings.defultSize * 15,
-                child: const Text(
-                  'Harry Potter and the Goblet of Fire',
-                  style: Styles.textStyle20,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ),
-            ],
-          )
+          BestSellerListViewItem(),
         ],
       ),
     );
