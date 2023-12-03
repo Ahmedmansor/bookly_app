f9becbc044b0836c0153ca989485629075f7f39e
Ahmedmansor <51694719+Ahmedmansor@users.noreply.github.com>
2023-12-03 02:34:20 +0200

    best seller item part two

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -11,7 +11,7 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
+      padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -22,9 +22,12 @@ class HomeViewBody extends StatelessWidget {
           ),
           const Text(
             'Best Seller',
-            style: Styles.titleMedium,
+            style: Styles.textStyle18,
           ),
-          BestSellerListViewItem(),
+          const SizedBox(
+            height: 20,
+          ),
+          const BestSellerListViewItem(),
         ],
       ),
     );
@@ -53,6 +56,22 @@ class BestSellerListViewItem extends StatelessWidget {
                   color: Colors.red),
             ),
           ),
+          const SizedBox(
+            width: 30,
+          ),
+          Column(
+            children: [
+              SizedBox(
+                width: AppSettings.defultSize * 15,
+                child: const Text(
+                  'Harry Potter and the Goblet of Fire',
+                  style: Styles.textStyle20,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
